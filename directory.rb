6ea188032c19c4a8edb2cd0d96813750447a68ad
your name <your email>
2018-10-22 13:18:58 +0000

    updated with comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "------------"
 puts "Dr Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freedy Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
 print "Overall, we have "
+#it's importnat that print() doesen't add a new line of characters
 print 11
 puts " great students"
