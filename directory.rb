3b2229495c9b2b998390e8dac2d996c4856132ef
your name <your email>
2018-10-22 14:23:47 +0000

    Takes user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# Let's put all the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 
 def print_header
 puts "The students of Villains Academy"
@@ -25,8 +11,20 @@ end
 def print_footer(names)
  puts "Overall, we have #{names.count} great students"
 end 
-#nothing happends until we call the methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end 
+  students 
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
-#updated to change commit to hash
