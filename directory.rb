009f38420ca2efc6a3b6845619387c8443ea0217
your name <your email>
2018-10-25 14:11:24 +0000

    saving student list to file

--- directory.rb
+++ directory.rb
@@ -1,8 +1,12 @@
 @students = [] 
+$count1 = 0
+$count = 1
+
 def print_header
 puts "The students of Villains Academy".center(40)
 puts "------------".center(40)
 end 
+
 def print_students_list(students)
     while true do 
       students.each do |student|
@@ -15,7 +19,8 @@ def print_students_list(students)
      end 
      break if students.length == $count - 1
     end 
-end 
+end
+
 def print_footer(names)
   if names.length == 0
     puts "We have no students\n"
@@ -25,6 +30,7 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great studentszn"
   end 
 end 
+
 def input_students
   def_cohort = "November"
   while true do 
@@ -64,11 +70,14 @@ def interactive_menu
     process(selection)
 end 
 end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
+
 def show_students
   print_header
   print_students_list(@students)
@@ -81,6 +90,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -88,11 +99,17 @@ def process(selection)
   end
 end
 	
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
-$count1 = 0
-$count = 1
+
 interactive_menu
-@students = input_students
-print_header
-print(students)
-print_footer(students)
